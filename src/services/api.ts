27290c807173f24a488d58525927f0155316971d
IRAMBONAEloi <eloirambonabbg@gmail.com>
2026-06-04 12:06:04 +0200

    api addition on backend URL

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'https://ims-a15g.onrender.com',
+    baseURL: 'https://ims-a15g.onrender.com/api',
     headers: {
         'Content-Type': 'application/json',
     },
